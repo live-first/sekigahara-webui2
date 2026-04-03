853bde5f9ad03abd9710da0058f69a15cdb5f10d
TakedaRyota <tkdryt05@gmail.com>
2026-04-03 21:34:05 +0900

    URL追加

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://sekigahara-idolwars.net/**')],
+    remotePatterns: [
+      new URL('https://sekigahara-idolwars.net/**'),
+      new URL('https://yosen2026.sekigahara-idolwars.net/**'),
+    ],
     unoptimized: true,
   },
   // output: 'export',
