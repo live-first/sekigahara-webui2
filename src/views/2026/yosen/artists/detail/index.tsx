7805d33ae122d7eb057cb811a3d454976205597a
TakedaRyota <tkdryt05@gmail.com>
2026-04-20 22:06:03 +0900

    20260420

--- src/views/2026/yosen/artists/detail/index.tsx
+++ src/views/2026/yosen/artists/detail/index.tsx
@@ -1,6 +1,8 @@
 import { Img } from '@/components/Image'
+import { Panel } from '@/components/Panel'
 import { artists } from '@/resource/2026/yosen/artists'
 import Link from 'next/link'
+import { FaLink } from 'react-icons/fa'
 
 export const YosenArtistsDetailView = (props: { id: string | number }) => {
   const { id } = props
@@ -11,15 +13,26 @@ export const YosenArtistsDetailView = (props: { id: string | number }) => {
     return <div>Artist not found</div>
   }
   return (
-    <div className='py-12'>
+    <div className='pb-12'>
       <Img src={detail.img.src} alt={detail.img.alt} />
-      <div className='text-sekigahara text-xl font-bold text-center h-15'>
+      <div className='text-sekigahara text-4xl font-bold text-center pt-4'>
         <p className='whitespace-pre-wrap h-full content-center'>{detail.name}</p>
+        {detail.yomi && <p className='text-black font-normal text-sm'>（読み：{detail.yomi}）</p>}
       </div>
-      <div className='text-black'>{detail.content}</div>
-      <div>
-        <video src={detail.movie} controls />
-      </div>
+      <Panel className='my-6 mx-2 md:mx-12 lg:mx-24'>
+        <div
+          className='whitespace-normal break-all text-black'
+          dangerouslySetInnerHTML={{
+            __html: `${detail.content}`,
+          }}
+        />
+      </Panel>
+
+      {detail.movie && (
+        <div className='flex justify-self-center max-w-4xl'>
+          <video src={detail.movie} controls />
+        </div>
+      )}
       <div className='flex gap-8 py-8 justify-center'>
         {detail.x && (
           <Link href={`https://x.com/${detail.x}`} className='bg-white rounded-full w-20 h-20 p-4'>
@@ -32,13 +45,19 @@ export const YosenArtistsDetailView = (props: { id: string | number }) => {
           </Link>
         )}
         {detail.tiktok && (
-          <Link href={detail.tiktok} className='bg-white rounded-full w-20 h-20 p-2 content-center'>
-            <Img src='https://lime-light.tv/images/youtube-logo.png' alt='youtube-logo' />
+          <Link
+            href={detail.tiktok}
+            className='bg-white rounded-full w-20 h-20 p-2 content-center justify-items-center'
+          >
+            <Img src='https://lime-light.tv/images/TikTok.png' alt='tiktok-logo' cName='w-4/5' />
           </Link>
         )}
         {detail.other && (
-          <Link href={detail.other} className='bg-white rounded-full w-20 h-20 p-2 content-center'>
-            <Img src='https://lime-light.tv/images/youtube-logo.png' alt='youtube-logo' />
+          <Link
+            href={detail.other}
+            className='bg-white rounded-full w-20 h-20 p-2 content-center justify-items-center'
+          >
+            <FaLink className='text-black w-4/5 h-4/5' />
           </Link>
         )}
       </div>
